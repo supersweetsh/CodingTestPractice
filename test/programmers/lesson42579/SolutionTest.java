cb66858fa759b0ea48312ef4ccac79a137aa5578
Sam <nicer2000@naver.com>
2022-03-18 11:22:33 +0900

    Init

---
+++ test/programmers/lesson42579/SolutionTest.java
@@ -0,0 +1,19 @@
+package programmers.lesson42579;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solution() {
+        String[] genres = new String[]{"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = new int[]{500, 600, 150, 800, 2500};
+
+        int[] result = new Solution().solution(genres, plays);
+        Assertions.assertArrayEquals(new int[]{4, 1, 3, 0}, result);
+    }
+
+}
